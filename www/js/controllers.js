81e0701aa66df539b7c64028dcdf67b3a191d6e2
Jason Melendez <jasonmc02@gmail.com>
2016-04-21 21:36:17 -0600

    Facebook removed, gps added, new ip

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,20 +1,29 @@
 var Users = [];
 angular.module('starter.controllers', ['ionic', 'starter.services'])
-.controller('LoginCtrl', function ($state, $scope, $stateParams, $ionicHistory) {
+.controller('LoginCtrl', function ($state, $scope, $stateParams, $ionicHistory, $http, $ionicLoading) {
   $ionicHistory.clearHistory();
   $scope.loginData = {};
   $scope.doLogin = function () {
     var data = $scope.loginData,
         re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if (!data.email || !data.password || !re.test(data.email)) {
-      alert("Invalid Email or Password");
+      alert('Invalid Email or Password');
     } else {
-      $state.go('app.users');
+      $ionicLoading.show({
+        noBackdrop: true,
+      });
+      $http
+      .post('http://www.crscript.com/sessions', {
+        email: data.email,
+        password: data.password
+      })
+      .then(function (response) {
+        $state.go('app.users');
+      }, function (error) {
+        $ionicLoading.hide();
+        alert('Something went wrong');
+      });
     }
-  };
-  $scope.doFacebookLogin = function ($event) {
-    $event.stopPropagation();
-    $event.preventDefault();
   };
 })
 .controller('MenuCtrl', function ($state, $scope, Camera) {
@@ -35,10 +44,8 @@ angular.module('starter.controllers', ['ionic', 'starter.services'])
     $state.go('login');
   };
 })
-.controller('UsersCtrl', function ($scope, $http, $ionicLoading) {
-  $ionicLoading.show({
-    noBackdrop: true,
-  });
+.controller('UsersCtrl', function ($scope, $http, $ionicLoading, $ionicHistory) {
+  $ionicHistory.clearHistory();
   $http
   .get('http://utn.herokuapp.com/api/users')
   .then(function (response) {
@@ -49,34 +56,51 @@ angular.module('starter.controllers', ['ionic', 'starter.services'])
     $ionicLoading.hide();
   });
 })
-.controller('UserCtrl', function ($scope, $stateParams, $cordovaGeolocation) {
+.controller('UserCtrl', function ($scope, $stateParams, $ionicLoading, $cordovaGeolocation) {
+  $ionicLoading.show({
+    noBackdrop: true,
+  });
   var userDetailled,
       userLocation,
+      posOptions,
+      myLatlng,
       mapOptions,
       marker,
-      userMap;
-
-  Users.map(function(user) {
+      userMap,
+      lat,
+      long;
+  Users.map(function (user) {
     if (user._id == $stateParams.id) {
       userDetailled = user;
     }
   });
-  myLatlng = new google.maps.LatLng(-34.397, 150.644),
-  mapOptions = { 
-    center: myLatlng,
-    zoom: 8,
-    timeout: 10000,
-    enableHighAccuracy: true,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  },
-  marker = new google.maps.Marker({
-    position: myLatlng,
-    title: userDetailled.name
+  posOptions = {timeout: 10000, enableHighAccuracy: true};
+  $cordovaGeolocation
+  .getCurrentPosition(posOptions)
+  .then(function (position) {
+    lat  = position.coords.latitude;
+    long = position.coords.longitude;
+    myLatlng = new google.maps.LatLng(lat, long),
+    mapOptions = { 
+      center: myLatlng,
+      zoom: 15,
+      timeout: 10000,
+      enableHighAccuracy: true,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    },
+    marker = new google.maps.Marker({
+      position: myLatlng,
+      title: userDetailled.name
+    });
+    userMap = new google.maps.Map(document.getElementById('map-container'), mapOptions);
+    marker.setMap(userMap);
+    $scope.map = userMap;
+  }, function (err) {
+    alert("Something went wrong");
+  })
+  .finally(function () {
+    $scope.user = userDetailled;
+    $ionicLoading.hide();
   });
-  userMap = new google.maps.Map(document.getElementById('map-container'), mapOptions);
-
-  marker.setMap(userMap);
-  $scope.user = userDetailled;
-  $scope.map = userMap;
 });
 
